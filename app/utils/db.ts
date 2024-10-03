1cdfb57b12dabde3a1d4bffa72af8151fd5b9e1f
Andrew Oak <andrewoaksoekhant@gmail.com>
2024-10-04 00:28:45 +0800

    UI overhaul + chat rooms

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -15,13 +15,26 @@ export function subscribeToChat(callback: () => {}) {
     .subscribe();
 }
 
-export async function getChats() {
+export async function getTopics() {
+  const { data, error } = await supabase.from("chats").select("topic");
+  if (data) {
+    return Array.from(new Set(data.map((item) => item.topic)));
+  }
+  return [];
+}
+
+export async function getChats(topic: string) {
   const { data, error } = await supabase
     .from("chats")
-    .select("username, message");
+    .select("username, message")
+    .eq("topic", topic);
   return data;
 }
 
-export async function insertChat(username: string, message: string) {
-  await supabase.from("chats").insert([{ username, message }]);
+export async function insertChat(
+  username: string,
+  message: string,
+  topic: string
+) {
+  await supabase.from("chats").insert([{ username, message, topic }]);
 }
