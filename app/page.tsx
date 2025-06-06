a0f521a2e2c0a7e461c9e1480eea343f30fd6cce
nicholasbay <bay.yong.wei@gmail.com>
2025-06-06 23:22:10 +0800

    Add navigation buttons to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
 import { TopBar } from "@/components/top-bar/top-bar";
 
 export default function HomePage() {
+  const router = useRouter();
+
+  const handleLoginClick = () => {
+    router.push("/login");
+  };
+
+  const handleSignUpClick = () => {
+    router.push("/sign-up");
+  };
+
   return (
     <div className="flex flex-col h-full w-full items-center gap-5">
-      <TopBar showLogo title="Home Page"/>
+      <TopBar showLogo title="GroupGPT">
+        <Button variant="ghost" onClick={ handleLoginClick }>Login</Button>
+        <Button variant="default" onClick={ handleSignUpClick }>Sign Up</Button>
+      </TopBar>
+
       <div className="flex justify-center items-center h-[calc(100%-80px)]">
         <p className="font-semibold">Welcome to GroupGPT</p>
       </div>
