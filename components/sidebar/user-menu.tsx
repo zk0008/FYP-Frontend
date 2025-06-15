879e4babb015e356a175dd580d1ff3f3138bf920
nicholasbay <bay.yong.wei@gmail.com>
2025-06-15 22:47:09 +0800

    Add notification dot for pending invites in user menu

--- components/sidebar/user-menu.tsx
+++ components/sidebar/user-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { CircleUser, ChevronsUpDown } from "lucide-react";
+import { CircleUser, ChevronsUpDown, Dot } from "lucide-react";
 
 import { AccountSettingsDialog, ManageInvitesDialog } from "@/components/dialogs/index";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
@@ -15,7 +15,7 @@ import {
   SidebarSeparator
 } from "@/components/ui/sidebar";
 import { TooltipWrapper } from "@/components/ui/tooltip-wrapper";
-import { useUserContext } from "@/hooks";
+import { useInvitesContext, useUserContext } from "@/hooks";
 
 import { SignOutButton } from "./sign-out-button";
 
@@ -23,6 +23,7 @@ export function UserMenu() {
   const [isAccountSettingsDialogOpen, setIsAccountSettingsDialogOpen] = useState<boolean>(false);
   const [isManageInvitesDialogOpen, setIsManageInvitesDialogOpen] = useState<boolean>(false);
   const { user } = useUserContext();
+  const { invites } = useInvitesContext();
 
   const userIcon = user?.username ? (
     <Avatar className="h-6 w-6">
@@ -44,7 +45,10 @@ export function UserMenu() {
               <SidebarMenuButton>
                 { userIcon }
                 { user?.username }
-                <ChevronsUpDown className="ml-auto" />
+                <div className="flex items-center ml-auto">
+                  { invites.length > 0 && <Dot className="h-8 w-8 text-red-500" /> }
+                  <ChevronsUpDown />
+                </div>
               </SidebarMenuButton>
             </DropdownMenuTrigger>
           </TooltipWrapper>
@@ -75,6 +79,7 @@ export function UserMenu() {
                 onClick={() => setIsManageInvitesDialogOpen(true)}
               >
                 <span>Manage Pending Invites</span>
+                { invites.length > 0 && <Dot className="h-8 w-8 text-red-500" /> }
               </Button>
               <ManageInvitesDialog
                 open={ isManageInvitesDialogOpen }
