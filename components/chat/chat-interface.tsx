844f84679237bf601834aaf811ed5311381d89df
nicholasbay <bay.yong.wei@gmail.com>
2025-08-17 17:49:43 +0800

    Change file drop zone for upload to knowledge base to MessageList

--- components/chat/chat-interface.tsx
+++ components/chat/chat-interface.tsx
@@ -7,17 +7,17 @@ import { FileDropZone } from "./file-drop-zone";
 export function ChatInterface() {
   return (
     <div className="flex flex-col h-full px-2">
-      <FileDropZone className="flex flex-col h-full">
-        <div className="flex-1 overflow-y-auto">
+      <div className="flex-1 overflow-y-auto">
+        <FileDropZone className="flex flex-col h-full">
           <MessagesList />
-        </div>
+        </FileDropZone>
+      </div>
 
         <ChatInputForm />
 
         <span className="text-xs text-center mt-1 pb-1 cursor-default">
           GroupGPT can make mistakes. Please verify important information.
         </span>
-      </FileDropZone>
     </div>
   );
 }
