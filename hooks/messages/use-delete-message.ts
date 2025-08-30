674f50361c5e99d26f8000eed86dd3ca860be13a
nicholasbay <bay.yong.wei@gmail.com>
2025-08-30 17:53:00 +0800

    Refactor useDeleteMessage hook to remove messageId parameter and streamline deleteMessage function

--- hooks/messages/use-delete-message.ts
+++ hooks/messages/use-delete-message.ts
@@ -1,71 +1,31 @@
 import { useCallback } from "react";
 
-import { createClient } from "@/utils/supabase/client";
+import { fetchWithAuth } from "@/utils";
 import { useUnifiedChatroomContext } from "@/hooks";
 
-interface useDeleteMessageProps {
+interface deleteMessageProps {
   messageId: string;
 }
 
-const supabase = createClient();
-
-export function useDeleteMessage({ messageId }: useDeleteMessageProps) {
+export function useDeleteMessage() {
   const { currentChatroom } = useUnifiedChatroomContext();
 
-  const deleteMessageAttachments = useCallback(async () => {
+  const deleteMessage = useCallback(async ({ messageId }: deleteMessageProps) => {
     if (!currentChatroom) {
       return { success: false, error: "Chatroom context not available." };
     }
 
-    try {
-      const { data: attachmentsData, error: attachmentsError } = await supabase
-        .from("attachments")
-        .select("attachment_id")
-        .eq("message_id", messageId);
+    const response = await fetchWithAuth(`/api/messages/${currentChatroom.chatroomId}/${messageId}`, {
+      method: "DELETE"
+    });
+    const data = await response.json()
 
-      if (attachmentsError) {
-        throw new Error(attachmentsError.message);
-      }
-
-      if (attachmentsData && attachmentsData.length > 0) {
-        const attachmentsToDelete = attachmentsData.map((attachment: any) => `${currentChatroom.chatroomId}/${attachment.attachment_id}`);
-
-        const { error } = await supabase
-          .storage
-          .from("attachments")
-          .remove(attachmentsToDelete);
-
-        if (error) {
-          throw new Error(error.message);
-        }
-      }
-
-      return { success: true, error: null };
-    } catch (error: any) {
-      console.error("Error deleting message attachments:", error);
-      return { success: false, error: error.message || "Failed to delete message attachments." };
+    if (!response.ok) {
+      return { success: false, error: data.message || "Failed to delete message." };
     }
-  }, [currentChatroom, messageId]);
 
-  const deleteMessage = useCallback(async () => {
-    try {
-      const { error: attachmentsError } = await deleteMessageAttachments();
-
-      const { error: messagesError } = await supabase
-        .from("messages")
-        .delete()
-        .eq("message_id", messageId);
-
-      if (attachmentsError || messagesError) {
-        throw new Error(attachmentsError?.message || messagesError?.message);
-      }
-
-      return { success: true, error: null };
-    } catch (error: any) {
-      console.error("Error deleting message:", error);
-      return { success: false, error: error.message || "Failed to delete the message." };
-    }
-  }, [messageId]);
+    return { success: true, error: null };
+  }, [currentChatroom]);
 
   return { deleteMessage };
 }
